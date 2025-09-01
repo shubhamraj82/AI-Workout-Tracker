3ff75ee93e8bd7db0b34281731b51b13f5fb539a
shubhamraj82 <raj.shubh8216@gmail.com>
2025-09-01 17:21:00 +0530

    exercise cards

--- src/app/components/ExerciseCard.tsx
+++ src/app/components/ExerciseCard.tsx
@@ -1,6 +1,8 @@
-import { View, Text, TouchableOpacity } from 'react-native'
+import { View, Text, TouchableOpacity, Image } from 'react-native'
 import React from 'react'
+import { urlFor } from '@/lib/sanity/client'
 import { Exercise } from '@/lib/sanity/types';
+import { Ionicons } from '@expo/vector-icons';
 
 const getDifficultyColor = (difficulty: string) => {
     switch (difficulty) {
@@ -44,7 +46,22 @@ export default function ExerciseCard({
     className='bg-white rounded-2xl mb-4 shadow-sm border border-gray-100'
     onPress={onPress}
     >
-      <Text>ExerciseCard</Text>
+      <View className='flex-row p-6'>
+        <View className='w-20 h-20 bg-white rounded-xl mr-4 overflow-hidden'>
+            {item.image ? (
+                <Image
+                source ={{uri:urlFor(item.image?.asset?._ref).url()}}
+                className='w-full h-full'
+                resizeMode="contain"
+                />
+            ) : (
+                <View className='w-full h-full bg-gradient-to-br from-blue-400 to-purple-500
+                items-center justify-center'>
+                    <Ionicons name='fitness' size={24} color="white"/>
+                </View>
+            )}
+        </View>
+      </View>
     </TouchableOpacity>
   );
 }
