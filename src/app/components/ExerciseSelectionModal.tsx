56461ee693ceeaa475479289afc2c67f04895871
shubhamraj82 <raj.shubh8216@gmail.com>
2025-09-06 05:25:21 +0530

    modal left

--- src/app/components/ExerciseSelectionModal.tsx
+++ src/app/components/ExerciseSelectionModal.tsx
@@ -1,7 +1,11 @@
-import { View, Text, Modal } from 'react-native'
+import { View, Text, Modal, StatusBar, TouchableOpacity, TextInput,RefreshControl,FlatList } from 'react-native'
 import React, { useState } from 'react'
 import { useRouter } from 'expo-router';
 import { useWorkoutStore } from 'store/workout-store';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Ionicons } from '@expo/vector-icons';
+import { set } from 'date-fns';
+import ExerciseCard from '@/app/components/ExerciseCard'
 
 interface ExerciseSelectionModalProps{
     visible:boolean;
@@ -19,7 +23,7 @@ export default function ExerciseSelectionModal({
     const [filteredExercises, setFilteredExercises] = useState<any[]>([]);
     const [refreshing, setRefreshing] = useState(false);
 
-    
+
   return (
     <Modal
     visible={visible}
@@ -27,7 +31,85 @@ export default function ExerciseSelectionModal({
     presentationStyle='pageSheet'
     onRequestClose={onClose}
     >
-        <Text>ExerciseSelectionModal</Text>
+        <SafeAreaView className='flex-1 bg-white'>
+            <StatusBar barStyle='dark-content'/>
+
+            {/* Header */}
+            <View className='bg-white px-4 pt-4 pb-6 shadow-sm border-b border-gray-100'>
+                <View className='flex-row items-center justify-between mb-4'>
+                    <Text className='text-2xl font-bold text-gray-800'>
+                        Add Exercise
+                    </Text>
+                    <TouchableOpacity
+                    onPress={onClose}
+                    className='w-8 h-8 items-center justify-center'
+                    >
+                        <Ionicons name='close' size={24} color="#6B7280"/>
+                    </TouchableOpacity>
+                </View>
+
+                <Text className='text-gray-600 mb-4'>
+                    Tap any exercise to add it to your workout
+                </Text>
+                {/* Search-bar */}
+                <View className='flex-row items-center bg-gray-100 rounded-xl px-4 py-3'>
+                    <Ionicons name='search' size={20} color="#6B7280"/>
+                    <TextInput
+                    className='flex-1 ml--3 text-gray-800'
+                    placeholder='Search exercises......'
+                    placeholderTextColor="9CA3AF"
+                    value={searchQuery}
+                    onChangeText={setSearchQuery}
+                    />
+                    {searchQuery.length > 0 && (
+                        <TouchableOpacity onPress={()=> setSearchQuery("")}>
+                            <Ionicons name='close-circle' size={20} color="#6B7280"/>
+                        </TouchableOpacity>
+                    )}
+                </View>
+            </View>
+
+            {/* Exercise-List */}
+            <FlatList 
+              data={filteredExercises}
+              renderItem={({ item }) => (
+                <ExerciseCard
+                  item={item}
+                  onPress={() =>handleExercisePress(item)}
+                    showChevron={false}
+                />
+              )}
+                keyExtractor={(item) => item._id}
+  showsVerticalScrollIndicator={false}
+  contentContainerStyle={{
+    padding:16,
+    paddingBottom:32,
+    paddingHorizontal:16,
+  }}
+              refreshControl={
+                <RefreshControl
+                refreshing={refreshing}
+                onRefresh={onRefresh}
+                colors={["#3B82F6"]} //android
+                tintColor="3B82F6" //ios
+                />
+              }
+              ListEmptyComponent={
+                <View className='bg-white rounded-2xl p-8 items-center'>
+                  <Ionicons className='text-outline' size={64} color="9CA3AF"/>
+                  <Text className='text-xl font-semibold text-gray-900 mt-4'>
+                    {searchQuery ? "no exercises found " : "Loading exercises..."}
+                  </Text>
+                  <Text className='text-gray-600 text-center mt-2'>
+                    {searchQuery
+                    ? "Try adjusting your search"
+                    : "your exercise will appear here"
+                    }
+                  </Text>
+                </View>
+              }
+            /> 
+        </SafeAreaView>
     </Modal>
   )
 }
